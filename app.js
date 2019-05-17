48118f0b2df24457576f0634753f20b60463b1a8
Fortur <andrey.daa.97@mail.ru>
2019-05-17 19:10:27 +0300

    rebuild mechanics api

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-
-let yandex = require('./app/getInfoFromYandex')
+let yandex = require('./app/getInfoFromYandex');
 
 let app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', yandex.getMainPage);
-
 app.listen('3000');
+
+yandex.getMainPage();
 
 module.exports = app;
--- app/getInfoFromYandex.js
+++ app/getInfoFromYandex.js
@@ -1,21 +1,59 @@
 const axios = require('axios');
 
-exports.getMainPage = (req, res) => {
-  let aLat = '55.85932288840268';
-  let aLong = '37.657724625662105';
-  let bLat = '55.876717';
-  let bLong = '37.662964';
-  // для аутентификации используется токен, который получается, пока не известным мне путём, либо csrfToken, с которым корректно работает Postman, но не работает axios почему-то
-  let token=''; // тут записывается токен. я получал его из схожих запросов, которые кидает http://fiddle.jshell.net/4xfxR/1/show/
-  async function f() {
-    const driverData = await axios.get(`https://api-maps.yandex.ru/services/route/2.0/?lang=ru_RU&token=${token}&rll=${aLong}%2C${aLat}~${bLong}%2C${bLat}`,
-      {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 YaBrowser/19.4.2.702 Yowser/2.5 Safari/537.36'
-        }
-      });
-    console.log(driverData.data.data.features[0].properties.RouteMetaData);
+exports.getMainPage = () => {
+  // let aLat = '55.85932288840268';
+  // let aLong = '37.657724625662105';
+  // let bLat = '55.876717';
+  // let bLong = '37.662964';
 
+  async function f1() {
+    const initReq = await axios.get(`https://yandex.ru/maps/api/router/buildRoute`, {}),
+      cookies = initReq.headers['set-cookie'].join(';'),
+      constcsrfToken = initReq.data.csrfToken.split(':'),
+      csrfTokenStart = constcsrfToken[0],
+      csrfTokenEnd = constcsrfToken[1],
+      testRLL = '37.60739779882813%2C55.82416031615465~37.620959047607435%2C55.79593314525986'; //coordinate of 2 points
+    // const rllExample = `${aLong}%2C${aLat}~${bLong}%2C${bLat}`;
+
+    async function getRouteInfo(type, rll) {
+      const driverData = await axios.get(`https://yandex.ru/maps/api/router/buildRoute?rll=${testRLL}&csrfToken=${csrfTokenStart}%3A${csrfTokenEnd}`,
+        {
+          params: {
+            lang: 'ru_RU',
+            mode: 'best',
+            results: '1',
+            type,
+          },
+          headers: {
+            Cookie: cookies
+          }
+        });
+
+      const dataAboutRoute = driverData.data.data.features[0].properties.RouteMetaData;
+      const resObject = {};
+
+      for (let key in dataAboutRoute) {
+        if (key === 'Distance' || key === 'WalkingDistance') {
+          resObject.Distance = dataAboutRoute[key].text;
+        } else if (key === 'Duration') {
+          resObject.Duration = dataAboutRoute[key].text;
+        } else if (key === 'type') {
+          resObject.Type = dataAboutRoute[key];
+        }
+      }
+
+      console.log(resObject);
+
+    }
+
+    await Promise.all([
+      getRouteInfo('auto'),
+      getRouteInfo('pedestrian'),
+      getRouteInfo('masstransit'),
+      getRouteInfo('bicycle'),
+    ]);
+    process.exit();
   }
-  f();
+
+  f1();
 };
